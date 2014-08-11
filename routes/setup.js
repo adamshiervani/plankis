5c1ec87046fce0c1538016afbe928c1b098cec57
Adam Shiervani <adam.shiervani@gmail.com>
2014-08-11 23:31:05 +0200

    Fix change in setup

--- routes/setup.js
+++ routes/setup.js
@@ -5,54 +5,40 @@ var TicketPatterns = require('./../schemas/ticket').ticketsPattern;
 exports.getConfiguration = function (req, res) {
 
 
-    if (!req.param('longitude') || !req.param('latitude') || !req.param('uuid')) {
+    if (!req.param('city') || !req.param('uuid')) {
         res.send(400, {error: 'Missing parameter!'});
         return;
     }
 
-    //Todo: remove this
-    var report = new TicketPatterns();
+
+    // report.save(function (err, save) {
+    //     console.log(save);
+    // });
+
+
+    report = new TicketPatterns();
     //Set all the Report variables
-    report.setCity('Berlin');
+    report.setCity(req.param('city'));
     report.setTicketPattern('pattern');
     report.setTicketFrom('1234');
 
     report.save(function (err, save) {
         console.log(save);
     });
+    var uuid = req.param('uuid');
 
+    res.send(200, {city: req.param('city'), ticketfrom:"asdasd", ticketpattern: "patter"})
 
-    report = new TicketPatterns();
-    //Set all the Report variables
-    report.setCity('Paris');
-    report.setTicketPattern('pattern');
-    report.setTicketFrom('1234');
-
-    report.save(function (err, save) {
-        // console.log(save);
-    });
-    var lat_origin = req.param('latitude'),
-    long_origin = req.param('longitude'),
-    uuid = req.param('uuid');
-
-    findArea.area(lat_origin, long_origin, function (err, area) {
-        console.log(area);
-        if (err || typeof(area) === "undefined" || typeof(area.neighborhoods) === "undefined" || area.neighborhoods.length === 0) {
-            res.send(401, null);
-            return;
-        }
-
-        TicketPatterns.findOne({city: area.neighborhoods[0].city}, {city: 1, ticketfrom: 1, ticketpattern: 1}, function (err, ticketPattern) {
-            console.log(ticketPattern);
-            if (err || ticketPattern === null || ticketPattern === {}) {
-                console.log(err);
-                res.send(401, err);
-                return;
-            }
-
-            res.send(200, ticketPattern);
-        });
-    });
+    // TicketPatterns.findOne({city: req.param('city')}, {city: 1, ticketfrom: 1, ticketpattern: 1}, function (err, ticketPattern) {
+    //     console.log(ticketPattern);
+    //     if (err || ticketPattern === null || ticketPattern === {}) {
+    //         console.log(err);
+    //         res.send(401, err);
+    //         return;
+    //     }
+    //
+    //     res.send(200, ticketPattern);
+    // });
 
 
 };
